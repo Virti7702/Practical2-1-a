10527e0cde92c4e03c2aecef94a6ae4f918b5ec0
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-13 18:02:29 +0530

    Add files via upload

---
+++ pratical2_1_a(1).py
@@ -0,0 +1,15 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - To check whether given key exists in dictionary or not.
+Github link - https://github.com/Virti7702/Practical2-1-a.git 
+
+'''
+
+
+dict = {1:'50', 2:'180', 3:'9'}
+a= 3
+if a in dict:
+    print("key is present in dictionary")
+else:
+        print("key is not present in dictainary")
